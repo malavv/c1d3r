e9133cbb2e01a4ff8b8abf49dfdc79ab8dfc22df
Maxime Lavigne (malavv) <duguigne@gmail.com>
2015-10-17 14:07:26 -0400

    Everything now in gulp.
      - Use gulp init when it's been some time.
      - gulp serve:dist or serve:dev can serve with auto-reload.

---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+(function() {
+  'use strict';
+
+  var
+    bower = require('gulp-bower'),
+    browserSync = require('browser-sync').create(),
+    del  = require('del'),
+    gulp = require('gulp'),
+    size = require('gulp-size'),
+    vulcanize = require('gulp-vulcanize'),
+    paths = {
+      pages: ['elements/**/*.html', 'elements/*.html', 'index.html'],
+      images: 'img/**/*',
+      allDist: ['dist/index.html', 'dist/img/**/*'],
+      distImages: 'dist/img/'
+    };
+
+  /**
+   * Default gulp action when just doing "$gulp".
+   *
+   * This should also include lint.
+   */
+  gulp.task('default', ['serve:dev']);
+
+  /** Allows serving the static pages right from here. */
+  gulp.task('serve:dev', [], function () {
+    browserSync.init({
+        server: {
+            baseDir: '.'
+        }
+    });
+    gulp.watch(paths.pages).on('change', browserSync.reload);
+  });
+
+  /** Updates bower if needed. */
+  gulp.task('init', function() {
+    return bower({ cmd: 'update'});
+  });
+
+  /** Serves the distribution version. */
+  gulp.task('serve:dist', ['dist'], function () {
+    browserSync.init({
+        server: {
+            baseDir: 'dist/'
+        }
+    });
+    gulp.watch(paths.allDist).on('change', browserSync.reload);
+  });
+
+  /** Recreates a distribution package. */
+  gulp.task('dist', ['copy:img', 'vulcanize']);
+
+  /** Cleans the distribution folder. */
+  gulp.task('clean:dist', function() {
+    return del(['dist/']);
+  });
+
+  /** Copies the img/ folder as it will stay as is. */
+  gulp.task('copy:img', function () {
+    return gulp.src(paths.images)
+      .pipe(gulp.dest(paths.distImages));
+  });
+
+  /**
+   * Vulcanize this web application.
+   *
+   * This means that all the pages and code will be merged in one single
+   * file.
+   */
+  gulp.task('vulcanize', function () {
+   return gulp.src('index.html')
+     .pipe(vulcanize({
+       stripComments: true,
+       inlineCss: true,
+       inlineScripts: true
+     }))
+     .pipe(size())
+     .pipe(gulp.dest('dist/'));
+  });
+})();
