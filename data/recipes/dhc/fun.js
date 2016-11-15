a6ce74eaa31277cb3fcb818bf679242cbc58429b
Maxime Lavigne (malavv) <duguigne@gmail.com>
2016-11-15 00:36:54 -0500

    Atributes

--- data/recipes/dhc/fun.js
+++ data/recipes/dhc/fun.js
@@ -17,33 +17,43 @@
   return {
     _acidAddInG(juicePh, finalPh, volumeInL, acidPka, acidMolarMass) {
       console.info('_acidAddInG', arguments);
+      return 4.00;
     },
     _addedVolumeOfSugarAddInL(og, chaptizationSugarAddKg, volumeInL, tg) {
       console.info('_addedVolumeOfSugarAddInL', arguments);
+      return 0.75;
     },
     _chaptizationSugarAddKg(degreeAlcohol, yeastEfficiency, og, sugarYieldPtsKgL, volumeInL) {
       console.info('_chaptizationSugarAddKg', arguments);
+      return 1.50;
     },
     _nutrientAddInG(volumeInL) {
       console.info('_nutrientAddInG', arguments);
+      return 3.00;
     },
     _pectinaseAddInG(volumeInL) {
       console.info('_pectinaseAddInG', arguments);
+      return 4.00;
     },
     _potassiumMetaAddInG(juicePh, finalPh, volumeInL) {
       console.info('_potassiumMetaAddInG', arguments);
+      return 5.00;
     },
     _primingSugarAddKg(sugarPpg, volumeInL) {
       console.info('_primingSugarAddKg', arguments);
+      return 0.127;
     },
     _sugarYieldPtsKgL(sugarPpg) {
       console.info('_sugarYieldPtsKgL', arguments);
+      return 46;
     },
     _tanninAddInG(origTanninLvl, desiredTanninLvl, volumeInL) {
       console.info('_tanninAddInG', arguments);
+      return 4.00;
     },
     _tg(og, sugarYieldPtsKgL, chaptizationSugarAddKg, volumeInL) {
       console.info('og', arguments);
+      return 1.023;
     }
   }
 })();
